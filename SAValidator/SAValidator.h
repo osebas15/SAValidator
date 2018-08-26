a168d07ddab3b72f0d351f5bbe5553c7f06ff7d9
Sebastian Aguirre <osebas15@gmail.com>
2018-08-26 12:51:05 -0400

    Initial Commit

---
+++ SAValidator/SAValidator.h
@@ -0,0 +1,19 @@
+//
+//  SAValidator.h
+//  SAValidator
+//
+//  Created by Sebastian Aguirre on 8/26/18.
+//  Copyright © 2018 SeaAnt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SAValidator.
+FOUNDATION_EXPORT double SAValidatorVersionNumber;
+
+//! Project version string for SAValidator.
+FOUNDATION_EXPORT const unsigned char SAValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SAValidator/PublicHeader.h>
+
+
